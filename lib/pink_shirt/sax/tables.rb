2a887568611b8f6eb4d09fe377192c1c58b91034
Graeme Worthy <graemeworthy@gmail.com>
2012-03-17 17:50:49 -0700

    tbody hung it, no longer

--- lib/pink_shirt/sax/tables.rb
+++ lib/pink_shirt/sax/tables.rb
@@ -1,12 +1,24 @@
 class PinkShirt
   class SAX::Tables < SAX::Base
-    TAGS = %w(table th tr td)
+    TAGS = %w(table th tr td tbody thead)
 
     def start_table attrs
+       @output << "\n"
        @output << "table#{add_attributes(attrs)}.\n" if add_attributes(attrs)
     end
 
     def end_table
+    end
+    def start_tbody attrs
+    end
+
+    def end_tbody
+    end
+
+    def start_thead attrs
+    end
+
+    def end_thead
     end
 
     def start_tr attrs
@@ -28,7 +40,7 @@ class PinkShirt
       if add_attributes(attrs)
         @output << "|#{add_attributes(attrs)}. "
       else
-        @output << "|"
+        @output << "| "
       end
     end
 
